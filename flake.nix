be6f8d5f8bcc32b62ee1fb2948078fac30ede6b5
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2024-07-31 22:06:00 -0600

    Work on flake for nix

--- flake.nix
+++ flake.nix
@@ -10,15 +10,37 @@
     utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
-      in {
+        dependencies = with pkgs; [
+          bash
+          gnused
+          gawk
+          git
+          direnv
+        ];
+      in rec {
+        packages = {
+          gh-worktree = pkgs.stdenv.mkDerivation {
+            name = "gh-worktree";
+            version = "0.1.0";
+            src = ./.;
+            buildInputs = dependencies;
+            nativeBuildInputs = [pkgs.makeWrapper];
+            phases = ["installPhase"];
+
+            installPhase = ''
+              mkdir -p $out
+              cp $src/README.md $out
+              makeWrapper "$src/gh-worktree" "$out/gh-worktree" --prefix PATH : ${
+                pkgs.lib.makeBinPath dependencies
+              }
+
+            '';
+          };
+        };
+        defaultPackage = packages.gh-worktree;
+
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            bash
-            sed
-            awk
-            git
-            direnv
-          ];
+          buildInputs = dependencies;
         };
       }
     );
